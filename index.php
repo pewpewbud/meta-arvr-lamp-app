98fa2006161cf347b2b518cc3a2cb677335a3009
pewp <117838302+pewpewbud@users.noreply.github.com>
2025-04-16 14:48:25 -0400

    Update index.php

--- index.php
+++ index.php
@@ -4,12 +4,10 @@
 <body>
     <h1>Meta AR/VR Marketplace Interface</h1>
 
-    <!-- Button 1: Launch HTML data view from Server 2 -->
     <form action="http://18.212.69.89/api_marketplace_raw.php" method="get" target="_blank">
         <button type="submit">View Marketplace Items (HTML)</button>
     </form>
 
-    <!-- Button 2: Launch raw API view from Server 2 -->
     <form action="http://18.212.69.89/api_marketplace.php" method="get" target="_blank">
         <button type="submit">View Marketplace API (JSON)</button>
     </form>
